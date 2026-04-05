d7639636e2fe9879d51baa43b78edb870c5e21c5
heitor jorge <heitorjorge39@gmail.com>
2026-04-05 16:19:01 -0300

    adição dos métodos de alterar e excluir

--- my-memories-api/Controllers/MyMemoriesController.cs
+++ my-memories-api/Controllers/MyMemoriesController.cs
@@ -22,11 +22,32 @@ namespace my_memories_api.Controllers
             return Ok(memories);
         }
 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetMemoryById(string id)
+        {
+            var memory = await _memoriesRepository.GetByIdAsync(id);
+            return Ok(memory);
+        }
+
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] Memory memory)
         {
             await _memoriesRepository.CreateAsync(memory);
             return Ok(memory);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(string id, Memory memory)
+        {
+            await _memoriesRepository.UpdateAsync(id, memory);
+            return Ok(memory);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            await _memoriesRepository.DeleteAsync(id);
+            return Ok();
         }
     }
 }
